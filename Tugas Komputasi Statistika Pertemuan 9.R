c97a1bab454a630877c901e78ab4476ecb8a81aa
dzakhwan <dgpkc17@gmail.com>
2026-05-13 17:31:22 +0700

    Proses Commit di R

---
+++ Tugas Komputasi Statistika Pertemuan 9.R
@@ -0,0 +1,19 @@
+# Tugas Komputasi Statistika Pertemuan 9
+# 1314624022_Dzakhwan Galih Pratama
+
+# Memuat Data
+data(mtcars)
+
+# Ringkasan Statistik
+summary(mtcars)
+
+# Hitung Rata-Rata dan Standar Deviasi MPG
+mean(mtcars$mpg)
+sd(mtcars$mpg)
+
+# Nilai Maksimum dan Minimum MPG
+max(mtcars$mpg)
+min(mtcars$mpg)
+
+# Melihat 6 data pertama MPG
+head(mtcars$mpg)
